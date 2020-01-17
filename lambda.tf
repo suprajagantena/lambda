a23bad563ebc0122eda9f63d0cefa69b9af99f3d
suprajagantena <suprajagantena@gmail.com>
2020-01-16 22:46:48 -0500

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -9,22 +9,7 @@ resource "aws_cloudwatch_log_group" "example" {
   retention_in_days = 14
 }
 
-#resource "aws_iam_policy" "lambda_logging" {
- #name = "lambda_logging"
-  #path = "/"
-  #description = "IAM policy for logging from a lambda"
 
-    {
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*",
-      "Effect": "Allow"
-    }
-  
-}
 #}
 
 #resource "aws_iam_role_policy_attachment" "lambda_logs" {
