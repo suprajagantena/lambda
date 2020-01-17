7571becd05235659263f53a61fdd8cb993f4c70b
suprajagantena <suprajagantena@gmail.com>
2020-01-16 22:29:40 -0500

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -1,11 +1,11 @@
 resource "aws_lambda_function" "test_lambda" {
-  function_name = "${var.lambda_function_name}"
+  function_name = "var.lambda_function_name"
  
   #depends_on    = ["aws_iam_role_policy_attachment.lambda_logs", "aws_cloudwatch_log_group.example"]
 }
 
 resource "aws_cloudwatch_log_group" "example" {
-  name              = "/aws/lambda/${var.lambda_function_name}"
+  name              = "/aws/lambda/var.lambda_function_name"
   retention_in_days = 14
 }
 
