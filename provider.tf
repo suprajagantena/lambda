2ee7cc43fadee0593baa38dd560fc6fbdf77c805
suprajagantena <suprajagantena@gmail.com>
2020-01-17 12:15:14 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,9 +3,9 @@ version = "~> 2.43"
 region  = "us-east-1"
 }
    
-assume_role {
- role_arn     = "arn:aws:iam::546366803473:role/jenkins"
- session_name = "TERRAFORM"
- external_id  = "EXTERNAL_ID"
- }
+ #assume_role {
+ #role_arn     = "arn:aws:iam::546366803473:role/jenkins"
+ #session_name = "TERRAFORM"
+ #external_id  = "EXTERNAL_ID"
+ #}
   
