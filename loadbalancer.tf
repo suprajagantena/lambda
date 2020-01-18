81053810a3f3af9b2a10b133d5d9fbe59a8782dd
suprajagantena <suprajagantena@gmail.com>
2020-01-18 18:29:40 -0500

    Update loadbalancer.tf

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -2,11 +2,11 @@ resource "aws_lb" "my_loadbalancer" {
   name               = "my_loadbalancer_tf"
   internal           = false
   load_balancer_type = "network"
-  subnets            = ""
+  subnets            = "aws_subnet.public.subnet-0c29fb2e17e09b02b"
 
   enable_deletion_protection = true
 
   tags = {
-    Environment = "production"
+    Environment = "dev"
   }
 }
