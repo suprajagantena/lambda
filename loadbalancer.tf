5410f49a0c9fd750c4f8229288504d2b86ee9c8e
suprajagantena <suprajagantena@gmail.com>
2020-01-18 18:34:53 -0500

    Update loadbalancer.tf

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "my_loadbalancer" {
   name               = "my_loadbalancer_tf"
   internal           = false
   load_balancer_type = "network"
-  subnets            = "aws_subnet.private.subnet-0c29fb2e17e09b02b"
+  subnets            = "aws_subnet-0c29fb2e17e09b02b"
 
   enable_deletion_protection = true
 
